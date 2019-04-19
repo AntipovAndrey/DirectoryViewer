0ceedaa4702c1cc12743063eb4b87c6d66498d2a
AntipovAndrey <antipov11.11@gmail.com>
2019-04-19 23:49:11 +0300

    [server]: implement controller for FileSystemService

---
+++ server/src/main/kotlin/com/github/antipovandrey/directoryviewer/controller/DirectoryViewerController.kt
@@ -0,0 +1,38 @@
+package com.github.antipovandrey.directoryviewer.controller
+
+import com.github.antipovandrey.directoryviewer.model.FileMetaData
+import com.github.antipovandrey.directoryviewer.service.FileSystemService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.HandlerMapping
+import javax.servlet.http.HttpServletRequest
+
+@RequestMapping("view")
+@RestController
+class DirectoryViewerController(
+        private val fileSystemService: FileSystemService
+) {
+
+    companion object {
+
+        private const val PATH_COMPONENTS_SEPARATOR = "/"
+        private const val WILDCARD_PATTERN = "**"
+    }
+
+    @GetMapping
+    fun getRootDescendants(): List<FileMetaData> {
+        return fileSystemService.getRootDescendants()
+    }
+
+    @GetMapping(WILDCARD_PATTERN)
+    fun getDescendantsForPath(request: HttpServletRequest): List<FileMetaData> {
+        val mappingPathComponents = request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE)
+                .toString()
+                .split(PATH_COMPONENTS_SEPARATOR)
+                .dropLast(1)
+        val wholeUriPathComponents = request.requestURI.split(PATH_COMPONENTS_SEPARATOR)
+        val filePathComponents = wholeUriPathComponents - mappingPathComponents
+        return fileSystemService.getDescendantsFor(filePathComponents)
+    }
+}
