cef9da2be0495f4c2f379ca06c423ec235e0a0f0
AntipovAndrey <antipov11.11@gmail.com>
2019-04-20 23:31:19 +0300

    [server]: fix file path misleading due to url encoding

--- server/src/main/kotlin/com/github/antipovandrey/directoryviewer/controller/DirectoryViewerController.kt
+++ server/src/main/kotlin/com/github/antipovandrey/directoryviewer/controller/DirectoryViewerController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.HandlerMapping
+import java.net.URLDecoder
 import javax.servlet.http.HttpServletRequest
 
 @RequestMapping
@@ -35,6 +36,7 @@ class DirectoryViewerController(
                 .split(PATH_COMPONENTS_SEPARATOR)
                 .dropLast(1)
         val wholeUriPathComponents = request.requestURI.split(PATH_COMPONENTS_SEPARATOR)
+                .map { URLDecoder.decode(it, "UTF-8"); }
         val filePathComponents = wholeUriPathComponents - mappingPathComponents
         log.info("Requested descendant for path: {} original request path: {}", filePathComponents, request.requestURI)
         return fileSystemService.getDescendantsFor(filePathComponents)
